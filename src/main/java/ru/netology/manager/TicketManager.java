722a3c3bf8355d6d3b96d4f8d4a31784c6beffcf
natabask <natabask@yandex.ru>
2022-03-21 21:04:35 +0300

    class TicketByPriceAscComparator added

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -4,6 +4,7 @@ import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class TicketManager {
     private TicketRepository repo;
@@ -16,14 +17,14 @@ public class TicketManager {
         repo.save(ticket);
     }
 
-    public Ticket[] findAll(String from, String to) {
+    public Ticket[] findAll(String from, String to, Comparator<Ticket> comparator) {
         Ticket[] result = new Ticket[0];
         for (Ticket ticket : repo.findAll()) {
             if ((ticket.getFrom() == from) && (ticket.getTo() == to)) {
                 Ticket[] tmp = new Ticket[result.length + 1];
                 System.arraycopy(result, 0, tmp, 0, result.length);
                 tmp[tmp.length - 1] = ticket;
-                Arrays.sort(tmp);
+                Arrays.sort(tmp, comparator);
                 result = tmp;
             }
         }
