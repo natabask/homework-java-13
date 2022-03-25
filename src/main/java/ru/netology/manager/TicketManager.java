6f52f8239ecaa2801a0c504d78d275fa64a36cbc
natabask <natabask@yandex.ru>
2022-03-25 10:22:19 +0300

    method findAll improved

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -5,6 +5,7 @@ import ru.netology.repository.TicketRepository;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Objects;
 
 public class TicketManager {
     private TicketRepository repo;
@@ -20,14 +21,14 @@ public class TicketManager {
     public Ticket[] findAll(String from, String to, Comparator<Ticket> comparator) {
         Ticket[] result = new Ticket[0];
         for (Ticket ticket : repo.findAll()) {
-            if ((ticket.getFrom() == from) && (ticket.getTo() == to)) {
+            if ((Objects.equals(ticket.getFrom(), from)) && (Objects.equals(ticket.getTo(), to))) {
                 Ticket[] tmp = new Ticket[result.length + 1];
                 System.arraycopy(result, 0, tmp, 0, result.length);
                 tmp[tmp.length - 1] = ticket;
-                Arrays.sort(tmp, comparator);
                 result = tmp;
             }
         }
+        Arrays.sort(result, comparator);
         return result;
     }
 }
